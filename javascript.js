7fdbcd0feb02d40c55d97d798ab1139ce74d00f9
chinandrew <chinandrew96@gmail.com>
2024-04-15 10:30:13 -0400

    Add + - signs for exposure and differentiate average absolute and average error

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const fstops = ["f1.4", "f1.6", "f1.8", "f2", "f2.2", "f2.5", "f2.8", "f3.2", "f3.5", "f4", "f4.5", "f5", "f5.6", "f6.3", "f7.1", "f8", "f9", "f10", "f11", "f13", "f14", "f16", "f18", "f20", "f22"];
 const numberOfQuestions = 10;
 let currentQuestion = 0;
+let absScore = 0;
 let score = 0;
 let questions = [];
 let answers = [];
@@ -47,13 +48,14 @@ function checkAnswer() {
     const feedbackElement = document.getElementById('feedback');
     let distance = 0;
     if (selectedAnswer === questions[currentQuestion].aperture) {
-        feedbackElement.textContent = `True aperture: ${correctAnswer}. Current score: ${Math.round(score*100)/100}.`;
+        feedbackElement.textContent = `True aperture: ${correctAnswer}. Current score: ${Math.round(absScore*100)/100}.`;
         feedbackElement.classList.add('correct');
         feedbackElement.classList.remove('incorrect');
     } else {
-        distance = Math.abs(fstops.indexOf(questions[currentQuestion].aperture) - fstops.indexOf(selectedAnswer))/3;
-        score += distance
-        feedbackElement.textContent = `True aperture: ${correctAnswer} (Error: ${Math.round(distance*100)/100} stops). Current score: ${Math.round(score*100)/100} stops.`;
+        distance = (fstops.indexOf(selectedAnswer) - fstops.indexOf(questions[currentQuestion].aperture))/3;
+        absScore += Math.abs(distance);
+        score += distance;
+        feedbackElement.textContent = `True aperture: ${correctAnswer} (Error: ${(distance<0?"":"+") + Math.round(distance*100)/100} stops). Current score: ${Math.round(absScore*100)/100} stops.`;
         feedbackElement.classList.add('incorrect');
         feedbackElement.classList.remove('correct');
     }
@@ -81,7 +83,7 @@ function showScore() {
     document.getElementById('answer').style.display = 'none';
     document.getElementById('feedback').style.display = 'none';
     const scoreElement = document.getElementById('score');
-    scoreElement.textContent = `Final score: ${Math.round(score*100)/100} stops. Average error:  ${Math.round(score/questions.length*100)/100} stops. `;
+    scoreElement.textContent = `Final score: ${Math.round(absScore*100)/100} stops. Average absolute error:  ${Math.round(absScore/questions.length*100)/100} stops.  Average error:  ${Math.round(score/questions.length*100)/100} stops. `;
     scoreElement.style.display = 'block';
     document.getElementById('nextButton').style.display = 'none'; // Hide Next button
     const restartButton = document.createElement('button');
@@ -89,6 +91,7 @@ function showScore() {
     restartButton.onclick = function() {
         document.getElementById('nextButton').textContent = 'Next'
         currentQuestion = 0;
+        absScore = 0;
         score = 0;
         scoreElement.style.display = 'none';
         document.getElementById('questionList').style.display = 'none'
@@ -102,7 +105,7 @@ function showScore() {
     questionListElement.innerHTML = '';
     questions.forEach((questionObj, index) => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${questionObj.make} ${questionObj.model}, ${questionObj.focal_length}mm, ${questionObj.exposure_time}s, ISO${questionObj.iso}, ${questionObj.aperture}. Selected ${questionObj.selectedAnswer} (Error: ${Math.round(questionObj.distance*100)/100} stops)`;
+        listItem.textContent = `${questionObj.make} ${questionObj.model}, ${questionObj.focal_length}mm, ${questionObj.exposure_time}s, ISO${questionObj.iso}, ${questionObj.aperture}. Selected ${questionObj.selectedAnswer} (Error: ${(questionObj.distance<0?"":"+") + Math.round(questionObj.distance*100)/100} stops)`;
         const image_link = document.createElement('a');
         image_link.href = questionObj.imageSource
         const image = document.createElement('img');
